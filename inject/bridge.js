cc52f1baafae98c2fe0342d23b5b18362125437c
Ninjaondra321 <ondran321@gmail.com>
2026-01-18 10:07:53 +0100

    Updated structure for easier cicd

--- inject/bridge.js
+++
@@ -1,1 +0,0 @@
-document.documentElement.setAttribute('player-url', chrome.runtime.getURL("pages/player.html"));
---
+++ src/inject/bridge.js
@@ -0,0 +1,1 @@
+document.documentElement.setAttribute('player-url', chrome.runtime.getURL("src/pages/player.html"));
